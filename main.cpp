27e1f33bea2eff73b419c790fec83b8ff5ae94c3
Matheus Victor <matheusvictor.salles@gmail.com>
2022-05-11 20:09:17 -0300

    WIP: Implementa ordenação por blocos

--- main.cpp
+++ main.cpp
@@ -7,25 +7,67 @@
 #include "headers\Ordenacao.h"
 #include "headers\Utilitarios.h"
 
+#define CAPACIDADE_MEMORIA 10
+
 using namespace std;
 
 int main(int argc, char** argv){
 
-    unsigned int numeroElementos = TAMANHO_MAXIMO;
+    unsigned int numeroElementos;
 
-    
     if (argc > 1) {
         numeroElementos = atoi(argv[1]);
-        if (numeroElementos > TAMANHO_MAXIMO) numeroElementos = TAMANHO_MAXIMO;
+    } else {
+        cout << "Quantos itens serao processados?" << endl;
+        cout << ">> ";
+        cin >> numeroElementos;
     }
-    
 
     Utilitarios utils;
-    Ordenadora ordenador;
     vector<int> vetorDesordenado = utils.gerarVetorDesordenado(numeroElementos);
+    vector<int> vetorFinal (numeroElementos);
+
+    Ordenadora ordenador;
     
-    utils.imprimirVetor(vetorDesordenado);
-    ordenador.ordenarPorMerge(vetorDesordenado);
-    utils.imprimirVetor(vetorDesordenado);
+    //utils.imprimirVetor(vetorDesordenado);
+
+    if (numeroElementos > CAPACIDADE_MEMORIA) {
+        int qtdBlocos = numeroElementos / CAPACIDADE_MEMORIA;        
+        cout << "Serao necessarios " << qtdBlocos << " blocos de processamento...";
+        
+        vector<int> vetorTemporario (qtdBlocos);
+
+        for (int i = 0; i < vetorTemporario.size(); i++) {
+            vetorTemporario.push_back(vetorDesordenado[i]);
+            if(i == 99) {
+                break;
+            }
+        }
+
+        for (int i = 0; i < vetorTemporario.size(); i++) {
+            if (vetorTemporario[i] != 0) 
+                cout << vetorTemporario[i] << " - ";
+        }
+
+        cout << " ====================" << endl;
+
+        ordenador.ordenarPorMerge(vetorTemporario);
+
+        for (int i = 0; i < vetorTemporario.size(); i++) {
+            if (vetorTemporario[i] != 0) 
+                cout << vetorTemporario[i] << " - ";
+        }
+
+    }
+
+    cout << "meu vetor gerado tem " << vetorDesordenado.size() << " elementos";
+    cout << "meu vetor final tem " << vetorFinal.size() << " elementos";
+
+    
+    
+    
+    
+    //ordenador.ordenarPorMerge(vetorDesordenado);
+    //utils.imprimirVetor(vetorDesordenado);
 
 }
